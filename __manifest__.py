ea803a55685e6eb2e4d473572c7e9586e2a35fab
BesaFX <islamhaker@gmail.com>
2019-05-27 15:14:39 +0200

    Change manifest.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "employee_car_request",
+    'name': "Employee Requests",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
